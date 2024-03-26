ec9a3a71bc6bfa275faa99207253e2467e4c6217
Larissa Sanches <larissaesanches@gmail.com>
2024-03-26 10:36:11 -0300

    CRUD Categoria

---
+++ src/main/java/com/generation/farmacia/model/Categoria.java
@@ -0,0 +1,46 @@
+package com.generation.farmacia.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categorias")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@NotNull(message = "O Atributo descricao é obrigatório")
+	@Size(min = 5, max = 50, message = "O atributo descricao deve conter no minimo 05 e no máximo 50 caracteres")
+	private String descricao;
+
+	
+
+	public Long getId() {
+		return id;
+	}
+
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+
+
+}
